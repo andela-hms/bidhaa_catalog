bfd0b327d25cd2cdaf0e819a06059634d08214fd
andela-hms <humphrey.musonye@andela.com>
2017-06-25 13:03:59 +0300

    feat(components): create the product_list component
    - create a template
    - add table to hold data
    - bootstrap it onto app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './products/product_list.component'
 
 @Component({
-    selector: 'pm-app',
+    selector: 'bd-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+    <div><h1>{{pageTitle}}</h1>
+        <bd-products></bd-products>
+    </div>
     `
 })
-export class AppComponent { }
+
+export class AppComponent {
+    pageTitle: string = 'Bidhaa Product Management'
+ }
---
+++ app/products/product_list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bd-products',
+    templateUrl: 'app/products/product_list.component.html'
+})
+
+export class ProductListComponent {
+
+}
